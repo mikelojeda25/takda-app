d1f96c8c76503eac8c75c0b8ade71ce4b2856e1c
Michael Joseph Ojeda <mikelojeda25@gmail.com>
2026-06-14 15:11:12 +0800

    chore: style

--- src/components/AlarmModal.jsx
+++ src/components/AlarmModal.jsx
@@ -51,7 +51,7 @@ export default function AlarmModal({ alarm, onSave, onClose }) {
 
         <div className="modal-body">
           <div className="form-group">
-            <label>Tasketeee</label>
+            <label>Title</label>
             <input
               type="text"
               placeholder="e.g. Daily Standup, Sprint Review..."
